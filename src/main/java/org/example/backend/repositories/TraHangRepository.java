2fb5ae724fd5729f2b332ddd1d2046391ce44ee6
Ina <anhlnph42539@fpt.edu.vn>
2025-01-14 03:17:20 +0700

    f

--- src/main/java/org/example/backend/repositories/TraHangRepository.java
+++ src/main/java/org/example/backend/repositories/TraHangRepository.java
@@ -12,6 +12,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -22,11 +23,12 @@ public interface TraHangRepository extends JpaRepository<TraHang, UUID> {
 
     @Query("SELECT COUNT(*) = SUM(CASE WHEN t.trangThai = :trangThai THEN 1 ELSE 0 END) " +
             "FROM TraHang t WHERE t.idHoaDon.id = :idHoaDon and t.idHoaDon.trangThai = :statusHd")
-    boolean areAllOrdersCompleted(@Param("idHoaDon") UUID idHoaDon, @Param("trangThai") String trangThai,@Param("statusHd") String statusHd);
+    boolean areAllOrdersCompleted(@Param("idHoaDon") UUID idHoaDon, @Param("trangThai") String trangThai, @Param("statusHd") String statusHd);
 
     @Query("SELECT h.trangThai FROM HoaDon h WHERE h.id = :idHoaDon and h.trangThai=:tt")
-    String findTrangThaiByIdHoaDon(@Param("idHoaDon") UUID idHoaDon ,String tt);
+    String findTrangThaiByIdHoaDon(@Param("idHoaDon") UUID idHoaDon, String tt);
 
 
     List<TraHang> findAllByIdHoaDon(HoaDon hoaDon);
+}
 
