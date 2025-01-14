7f922ecc3d238bac25d4f3bf249e0dda4e758101
amitycvn <c.amity.vn@gmail.com>
2025-01-14 21:52:44 +0700

    cap nhat tra hang client

--- src/main/java/org/example/backend/repositories/SanPhamRepository.java
+++ src/main/java/org/example/backend/repositories/SanPhamRepository.java
@@ -3,12 +3,17 @@ package org.example.backend.repositories;
 import jakarta.transaction.Transactional;
 import org.example.backend.dto.response.SanPham.SanPhamDetailRespon;
 import org.example.backend.dto.response.SanPham.SanPhamResponse;
+import org.example.backend.models.ChatLieu;
+import org.example.backend.models.DanhMuc;
+import org.example.backend.models.DeGiay;
+import org.example.backend.models.LopLot;
 import org.example.backend.models.SanPham;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -59,6 +64,24 @@ public interface SanPhamRepository extends JpaRepository<SanPham, UUID> {
             """)
     Page<SanPhamResponse> phanTrang(Pageable pageable);
 
-
-    List<SanPham> findAllByTrangThai(String trangThai);
+//    Page<SanPham> findAllByMaAndTenAndIdChatLieuAndIdLopLotAndIdDeGiayAndIdDanhMuc(
+//            Pageable pageable, String ma, String ten, ChatLieu idChatLieu, LopLot idLopLot, DeGiay idDeGiay, DanhMuc idDanhMuc);
+@Query("SELECT sp FROM SanPham sp " +
+       "WHERE (:ma IS NULL OR LOWER(sp.ma) LIKE LOWER(CONCAT('%', :ma, '%'))) " +
+       "AND (:ten IS NULL OR LOWER(sp.ten) LIKE LOWER(CONCAT('%', :ten, '%'))) " +
+       "AND (:idChatLieu IS NULL OR sp.idChatLieu.id = :idChatLieu) " +
+       "AND (:idLopLot IS NULL OR sp.idLopLot.id = :idLopLot) " +
+       "AND (:idHang IS NULL OR sp.idHang.id = :idHang) " +
+       "AND (:idDeGiay IS NULL OR sp.idDeGiay.id = :idDeGiay) " +
+       "AND (:idDanhMuc IS NULL OR sp.idDanhMuc.id = :idDanhMuc) " +
+       "AND sp.deleted = FALSE")
+Page<SanPham> search(
+        @Param("ma") String ma,
+        @Param("ten") String ten,
+        @Param("idChatLieu") UUID idChatLieu,
+        @Param("idLopLot") UUID idLopLot,
+        @Param("idHang") UUID idHang,
+        @Param("idDeGiay") UUID idDeGiay,
+        @Param("idDanhMuc") UUID idDanhMuc,
+        Pageable pageable);
 }
